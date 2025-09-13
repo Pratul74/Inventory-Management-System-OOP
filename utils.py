584837284197bb447dfbebb169db823c402d3d9c
Pratul Kumar <pratulkumar2003@gmail.com>
2025-09-13 15:33:12 +0530

    Added Inventory class

--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
-class Person:
-    def __init__(self,fullname,username,age,phonenumber):
+class Staff:
+    def __init__(self, fullname, username, staffId, age, phonenumber):
         self.fullname=fullname
         self.username=username
         self.age=age
         self.phonenumber=phonenumber
+        self.staffId=staffId
+
 
     def __str__(self):
-        return f"Name: '{self.fullname}', username: '{self.username}', Age: '{self.age}', Phone Number: '{self.phonenumber}'"
+        return f"Name: '{self.fullname}', username: '{self.username}', Staff Id: '{self.staffId}', Age: '{self.age}', Phone Number: '{self.phonenumber}'"
 
 class Product:
     def __init__(self, name, category, price, quantity):
@@ -17,6 +19,24 @@ class Product:
     
     def update_price(self,price):
         self.price=price
+
+
+class Inventory:
+
+    def add_staff(self):
+        pass
+
+    def add_product(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+
+
+    
+    
 
 
 
@@ -30,6 +50,8 @@ class Product:
 # icecream.update_price(120)
 # print(icecream.price)
 
-# pratul=Person("Pratul Kumar","Pratul74",22,"7004644521")
-# print(pratul)
+# pratul=Staff("Pratul Kumar",101,"Pratul74",22,"7004644521")
+# inventory=Inventory(pratul,icecream)
+# inventory.add_supply(icecream,500)
+
     
